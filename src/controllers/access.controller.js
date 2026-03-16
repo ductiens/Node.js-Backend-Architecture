79de864b75aef86fc9058f4eee548c1142bdfb88
ductiens <carina.ndt@gmail.com>
2026-03-16 10:33:43 +0700

    Make Your API Success Response use class

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,12 +1,22 @@
+import { CREATED } from "../core/success.response.js";
 import { signup } from "../services/access.service.js";
 
 export const signUp = async (req, res, next) => {
   // try {
-    const result = await signup(req.body);
-    return res.status(201).json(result);
+  //  const result = await signup(req.body);
+  //  return res.status(201).json(result);
   // } catch (error) {
   //   next(error);
   // }
+
+  new CREATED({
+    message: "Registered OK!",
+    metadata: await signup(req.body),
+    // options: {
+    //   limit: 10,
+    //   page: 1,
+    // },
+  }).send(res);
 };
 
 export const login = async (req, res, next) => {
